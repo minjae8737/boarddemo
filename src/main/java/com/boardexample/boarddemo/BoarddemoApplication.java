9ec79a1f30c81d3383dda1caa52f2dabe6668f38
Aljae <minjae8737@gmail.com>
2024-01-03 10:08:38 +0900

    Initial commit

---
+++ src/main/java/com/boardexample/boarddemo/BoarddemoApplication.java
@@ -0,0 +1,13 @@
+package com.boardexample.boarddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoarddemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoarddemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boardexample/boarddemo/BoarddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boardexample.boarddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoarddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
